4bfbd8a183d1d36a5cf77dff96c34938d5627a74
Joel Ring <joelwofo@users.noreply.github.com>
2020-09-18 12:54:38 +0200

    works to land!

--- Assets/Scripts/FlyByWire.cs
+++ Assets/Scripts/FlyByWire.cs
@@ -4,76 +4,82 @@ using UnityEngine;
 
 public class FlyByWire : MonoBehaviour
 {
+    public GameHandler GameHandler;
+    
     public Thruster t1, t2, t3, t4;
     
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
-    {   
-        if (Input.GetAxis("FwdBck") < 0)
+    {
+        if (GameHandler.currentState != GameHandler.gameState.GameOver &&
+            GameHandler.currentState != GameHandler.gameState.Crashed)
         {
-            Debug.Log("moving down");
-            t1.enabled = true;
-            t2.enabled = true;
-            t3.enabled = false;
-            t4.enabled = false;
-        }
-        else if (Input.GetAxis("FwdBck") > 0)
-        {
-            Debug.Log("moving up");
-            t1.enabled = false;
-            t2.enabled = false;
-            t3.enabled = true;
-            t4.enabled = true;
-        }
-        else if (Input.GetAxis("StrafeShip") < 0)
-        {
-            Debug.Log("moving left");
-            t1.enabled = false;
-            t2.enabled = true;
-            t3.enabled = false;
-            t4.enabled = true;
-            ;
-        }
-        else if (Input.GetAxis("StrafeShip") > 0)
-        {
-            Debug.Log("moving right");
-            t1.enabled = true;
-            t2.enabled = false;
-            t3.enabled = true;
-            t4.enabled = false;
-            ;
-        }
-        else if (Input.GetAxis("YawShip") < 0)
-        {
-            Debug.Log("yawing counter clockwise");
-            t1.enabled = false;
-            t2.enabled = true;
-            t3.enabled = true;
-            t4.enabled = false;
-        }
-        else if (Input.GetAxis("YawShip") > 0)
-        {
-            Debug.Log("yawing clockwise");
-            t1.enabled = true;
-            t2.enabled = false;
-            t3.enabled = false;
-            t4.enabled = true;
-        }
-        else
-        {
-            t1.enabled = Input.GetButton("Thruster1");
-            t2.enabled = Input.GetButton("Thruster2");
-            t3.enabled = Input.GetButton("Thruster3");
-            t4.enabled = Input.GetButton("Thruster4");
+
+
+            if (Input.GetAxis("FwdBck") < 0)
+            {
+                Debug.Log("moving down");
+                t1.enabled = true;
+                t2.enabled = true;
+                t3.enabled = false;
+                t4.enabled = false;
+            }
+            else if (Input.GetAxis("FwdBck") > 0)
+            {
+                Debug.Log("moving up");
+                t1.enabled = false;
+                t2.enabled = false;
+                t3.enabled = true;
+                t4.enabled = true;
+            }
+            else if (Input.GetAxis("StrafeShip") < 0)
+            {
+                Debug.Log("moving left");
+                t1.enabled = false;
+                t2.enabled = true;
+                t3.enabled = false;
+                t4.enabled = true;
+                ;
+            }
+            else if (Input.GetAxis("StrafeShip") > 0)
+            {
+                Debug.Log("moving right");
+                t1.enabled = true;
+                t2.enabled = false;
+                t3.enabled = true;
+                t4.enabled = false;
+                ;
+            }
+            else if (Input.GetAxis("YawShip") < 0)
+            {
+                Debug.Log("yawing counter clockwise");
+                t1.enabled = false;
+                t2.enabled = true;
+                t3.enabled = true;
+                t4.enabled = false;
+            }
+            else if (Input.GetAxis("YawShip") > 0)
+            {
+                Debug.Log("yawing clockwise");
+                t1.enabled = true;
+                t2.enabled = false;
+                t3.enabled = false;
+                t4.enabled = true;
+            }
+            else
+            {
+                t1.enabled = Input.GetButton("Thruster1");
+                t2.enabled = Input.GetButton("Thruster2");
+                t3.enabled = Input.GetButton("Thruster3");
+                t4.enabled = Input.GetButton("Thruster4");
+            }
         }
 
-     
         //.... continye the same way with the rest of the axes
         //.... SOOO much if/else. must be a smarter way to do that.
     }
--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -1,18 +1,116 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UIElements;
 
 public class GameHandler : MonoBehaviour
 {
+    public enum gameState {
+        Start,
+        Flying, 
+        Landing,
+        GameOver, 
+        Crashed
+    }
+    public gameState currentState = gameState.Start;
+    
+    private Vector2 startPosition;
+    private float startRotation;
+    public float maxLandingDistance = 3.0f;
+    public float maxLandingVelocity = 0.3f;
+
+    public Rigidbody2D spaceShipRB;
+    public GameObject landingZone;
+    
+    public Collider2D landingLeg1Collider;
+    public Collider2D landingLeg2Collider;
+    public Collider2D landingZoneCollider;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = spaceShipRB.position;
+        startRotation = spaceShipRB.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
+        switch (currentState)
+        {
+            case gameState.Start:
+                Debug.Log("state:start");
+                //reset spaceship position
+                spaceShipRB.position = startPosition;
+                spaceShipRB.rotation = startRotation;
+                
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    currentState = gameState.Flying;
+                }
+                spaceShipRB.velocity = new Vector2(0,0);
+                break;
+            case gameState.Flying:
+                Debug.Log("state:flying");
+                //land if we are close enough and slow enough and correctly aligned
+                if (Vector2.Distance(spaceShipRB.position, landingZone.transform.position) < maxLandingDistance)
+                {
+                    currentState = gameState.Landing;
+                    //display landing message
+                }
+                break;
+
+            case gameState.Landing:
+                //Debug.Log("state:landing");
+
+                if (Vector2.Distance(spaceShipRB.position, landingZone.transform.position) > maxLandingDistance)
+                {
+                    currentState = gameState.Flying;
+                    //display flying message
+                }
+
+               
+                //land if we are close enough and slow enough and correctly aligned
+                if (landingLeg1Collider.Distance(landingZoneCollider).isOverlapped &&
+                    landingLeg2Collider.Distance(landingZoneCollider).isOverlapped
+                    )
+                {
+                    if (spaceShipRB.velocity.magnitude < maxLandingVelocity)
+                    {
+                        currentState = gameState.GameOver;
+                    }
+                    else
+                    {
+                        currentState = gameState.Crashed;
+                    }
+                        
+                }
+                break;
+            
+            case gameState.GameOver:
+                Debug.Log("state:game over");
+                spaceShipRB.velocity = new Vector2(0,0);
+                
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    currentState = gameState.Start;
+                }
+                //show game over message
+                break;
+            
+            case gameState.Crashed:
+                Debug.Log("state:crashed");
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    currentState = gameState.Start;
+                }
+                //show game over message
+                break;
+            
+            default:
+                break;
+        }
         
     }
 }
